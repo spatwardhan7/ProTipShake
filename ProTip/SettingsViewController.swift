6d2e18aa4f2667a69880e0ab8882f05810989e54
Saurabh Patwardhan <saurabh.patwardhan89@gmail.com>
2016-02-23 01:19:12 -0800

    Added files via upload

--- ProTip/SettingsViewController.swift
+++ ProTip/SettingsViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    let DEFAULT_TIP_KEY = "DEFAULT TIP KEY"
+    let DEFAULT_VALUE_CHANGED = "DEFAULT VALUE CHANGED"
+    let DARK_THEME_KEY = "DARK THEME KEY"
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
 
+    @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var defaultTipPercentLabel: UILabel!
+    @IBOutlet weak var defaultTipSlider: UISlider!
+    @IBOutlet weak var jokeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Settings View didLoad")
+        themeSwitch.on = defaults.boolForKey(DARK_THEME_KEY)
+        setTheme(themeSwitch.on)
+        jokeLabel.alpha = 0
+        defaults.setBool(false, forKey: DEFAULT_VALUE_CHANGED)
+        let defaultTipVal = defaults.integerForKey(DEFAULT_TIP_KEY)
+        defaultTipPercentLabel.text = defaultTipVal.description + "%"
+        defaultTipSlider.value = Float(defaultTipVal)
+        setTipColor()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +41,98 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.becomeFirstResponder()
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            print("Shake detected!")
+            makeAPICall()
+        }
+    }
+    
+    @IBAction func onTipSliderValueChanged(sender: AnyObject) {
+        let tipPercentString = Int(roundValue(defaultTipSlider)).description + "%"
+        defaultTipPercentLabel.text = tipPercentString
+        setTipColor()
+        defaults.setInteger(Int(roundValue(defaultTipSlider)), forKey: DEFAULT_TIP_KEY)
+        defaults.setBool(true, forKey: DEFAULT_VALUE_CHANGED)
+    }
+    
+    @IBAction func onSwitchToggle(sender: AnyObject) {
+        setTheme(themeSwitch.on)
+        defaults.setBool(themeSwitch.on, forKey: DARK_THEME_KEY)
+    }
+    
+    func setTheme(isDark: Bool){
+        if isDark {
+            view.backgroundColor = UIColor.lightGrayColor()
+        }
+        else {
+            view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    func roundValue(slider: UISlider) -> Float {
+        return roundf(slider.value);
+    }
+    
+    func setTipColor(){
+        let tipPercentage = Double(roundValue(defaultTipSlider))
+        if tipPercentage <= 15 {
+            defaultTipPercentLabel.textColor = UIColor.greenColor()
+        } else if tipPercentage <= 20 {
+            defaultTipPercentLabel.textColor = UIColor.orangeColor()
+        } else {
+            defaultTipPercentLabel.textColor = UIColor.redColor()
+        }
+    }
+    
+    func makeAPICall(){
+        let postEndpoint: String = "http://api.icndb.com/jokes/random?limitTo=[nerdy]"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: postEndpoint)!
+        
+        session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            do {
+                if let responseString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
+                    // Print what we got from the call
+                    print(responseString)
+                    
+                    // Parse the JSON to get the Joke
+                    let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                    let value = jsonDictionary["value"] as! NSDictionary
+                    let joke = value["joke"] as! String
+                    // Update the label
+                    self.performSelectorOnMainThread("updateJokeLabel:", withObject: joke, waitUntilDone: false)
+                    print(joke)
+                }
+            } catch {
+                print("bad things happened")
+            }
+        }).resume()
+    }
+    
+    func updateJokeLabel(joke: String){
+        self.jokeLabel.alpha = 1
+        self.jokeLabel.text = joke
+        self.jokeLabel.numberOfLines = 0
+        self.jokeLabel.sizeToFit()
+    }
     /*
     // MARK: - Navigation
 
--- ProTip/TipViewController.swift
+++ ProTip/TipViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TipViewController.swift
 //  ProTip
 //
 //  Created by Patwardhan, Saurabh on 2/21/16.
@@ -8,41 +8,210 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
 
+    let DEFAULT_TIP_KEY  = "DEFAULT TIP KEY"
+    let LAST_BILL_AMOUNT = "LAST BILL AMOUNT"
+    let LAST_TIP_INDEX = "LAST TIP INDEX"
+    let LAST_CLOSED_DATE = "LAST CLOSED DATE"
+    let DEFAULT_VALUE_CHANGED = "DEFAULT VALUE CHANGED"
+    let RESTORED_STATE_KEY = "RESTORED STATE KEY"
+    let DARK_THEME_KEY = "DARK THEME KEY"
+    let LAUNCHED_BEFORE = "LAUNCHED BEFORE"
+
+    let TIME_INTEVAL_FOR_RECONSTRUCT : Double = 600
+    let DEFAULT_TIP : Int = 15
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var restoredState = false
+    
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var tipHeaderLabel: UILabel!
+    @IBOutlet weak var totalHeaderLabel: UILabel!
+    @IBOutlet weak var separatorBarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ProTip view did load")
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "0.00"
+        
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "resigningActive",
+            name: UIApplicationWillResignActiveNotification,
+            object: nil
+        )
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "becomeActive",
+            name: UIApplicationDidBecomeActiveNotification,
+            object: nil
+        )
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "terminateActive",
+            name: UIApplicationWillTerminateNotification,
+            object: nil
+        )
+        checkLastClosed()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        billField.becomeFirstResponder()
+        
+        
+        let isFirstLaunch = !defaults.boolForKey(LAUNCHED_BEFORE)
+        if isFirstLaunch {
+            defaults.setInteger(DEFAULT_TIP, forKey: DEFAULT_TIP_KEY)
+            tipPercentSlider.value = Float(DEFAULT_TIP)
+            updateUI()
+            defaults.setBool(true, forKey: LAUNCHED_BEFORE)
+        }
+        
+        applyTheme()
+        restoredState = defaults.boolForKey(RESTORED_STATE_KEY)
+        if !restoredState {
+            setDefaults()
+        }
+        defaults.setBool(false, forKey: RESTORED_STATE_KEY)
+    }
+    
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18,0.20,0.22]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        updateUI()
+    }
+
+    @IBAction func onSliderValueChanged(sender: AnyObject) {
+        updateUI()
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+    }
+    
+    func resigningActive(){
+        saveCurrentState()
+    }
+    
+    func terminateActive(){
+        saveCurrentState()
+    }
+    
+    func becomeActive(){
+        checkLastClosed()
+    }
+    
+    func setDefaults(){
+        let valueChanged = defaults.boolForKey(DEFAULT_VALUE_CHANGED)
+        if valueChanged {
+            let defaultTip = defaults.integerForKey(DEFAULT_TIP_KEY)
+            tipPercentSlider.value = Float(defaultTip)
+            updateUI()
+        }
+        
+    }
+    
+    func saveCurrentState(){
+        print("Saving Current State")
+        defaults.setObject(billField.text, forKey: LAST_BILL_AMOUNT)
+        defaults.setInteger(Int(roundValue(tipPercentSlider)), forKey: LAST_TIP_INDEX)
+        defaults.setObject(NSDate(), forKey: LAST_CLOSED_DATE)
+    }
+    
+    func checkLastClosed(){
+        let lastDate = defaults.objectForKey(LAST_CLOSED_DATE) as? NSDate
+        if lastDate != nil {
+            let currentDate = NSDate()
+            let timeInterval = currentDate.timeIntervalSinceDate(lastDate!)
+            if timeInterval < TIME_INTEVAL_FOR_RECONSTRUCT {
+                print("Time Interval: \(timeInterval)")
+                let billAmount = defaults.objectForKey(LAST_BILL_AMOUNT) as? String
+                if billAmount != nil {
+                    defaults.setBool(true, forKey: RESTORED_STATE_KEY)
+                    billField.text = billAmount
+                    tipPercentSlider.value = Float(defaults.integerForKey(LAST_TIP_INDEX))
+                    updateUI()
+                }
+            }
+            else {
+                print("Time Interval: \(timeInterval)")
+                billField.text = ""
+                tipPercentSlider.value = Float(defaults.integerForKey(DEFAULT_TIP_KEY))
+                updateUI()
+            }
+        }
+        else {
+            print("lastDate is nil")
+        }
+    }
+    
+    func applyTheme(){
+        if defaults.boolForKey(DARK_THEME_KEY) {
+            view.backgroundColor = UIColor.lightGrayColor()
+        } else{
+            view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    func updateUI(){
+        let tipPercentage = Double(roundValue(tipPercentSlider))
+        
         let billAmount = billField.text!._bridgeToObjectiveC().doubleValue
-        let tip = billAmount * tipPercentage
+        let tip = billAmount * (tipPercentage * 0.01)
         let total = billAmount + tip
         
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
+        let numberFortmatter = NSNumberFormatter()
+        numberFortmatter.numberStyle = .CurrencyStyle
+        tipLabel.text = numberFortmatter.stringFromNumber(tip)
+        totalLabel.text = numberFortmatter.stringFromNumber(total)
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        let tipPercentString = Int(roundValue(tipPercentSlider)).description + "%"
         
+        tipPercentLabel.text = tipPercentString
+        if tipPercentage <= 15 {
+            tipPercentLabel.textColor = UIColor.greenColor()
+            tipLabel.textColor = UIColor.greenColor()
+        } else if tipPercentage <= 20 {
+            tipPercentLabel.textColor = UIColor.orangeColor()
+            tipLabel.textColor = UIColor.orangeColor()
+        } else {
+            tipPercentLabel.textColor = UIColor.redColor()
+            tipLabel.textColor = UIColor.redColor()
+        }
+        
+        if total > 0 {
+            showHiddenFields(true)
+        } else {
+            showHiddenFields(false)
+        }
     }
-
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+    
+    func showHiddenFields(show: Bool){
+        if show {
+            tipHeaderLabel.alpha = 1
+            tipLabel.alpha = 1
+            separatorBarView.alpha = 1
+            totalHeaderLabel.alpha = 1
+            totalLabel.alpha = 1
+        } else {
+            tipHeaderLabel.alpha = 0
+            tipLabel.alpha = 0
+            separatorBarView.alpha = 0
+            totalHeaderLabel.alpha = 0
+            totalLabel.alpha = 0
+        }
+    }
+    
+    func roundValue(slider: UISlider) -> Float {
+        return roundf(slider.value);
     }
 }
-
