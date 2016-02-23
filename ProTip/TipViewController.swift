0a06ce550ce3664fe1b20557668cb7e9db7c58f6
spatwardhan7 <saurabh.patwardhan89@gmail.com>
2016-02-23 01:11:54 -0800

    commit to git

---
+++ ProTip/TipViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ProTip
+//
+//  Created by Patwardhan, Saurabh on 2/21/16.
+//  Copyright © 2016 honeybadgerinc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18,0.20,0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let billAmount = billField.text!._bridgeToObjectiveC().doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
